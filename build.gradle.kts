1936e682bf8488dbbe7abf6ae6eeb2de4e9798a1
NikkyAi <root@nikky.moe>
2018-10-07 08:58:40 +0200

    clean imports and unused stuff from gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,13 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.io.FilenameFilter
-
 plugins {
     id("voodoo") version "0.4.0"
 }
 
 voodoo {
 //    generatedSource = project.file(".src")
+//    packDirectory = project.file("packs")
 }
 
 // only required for plugin dev
-repositories {
-    mavenLocal()
-}
-
-//kotlin.sourceSets.asMap.map { (name, source) ->
-//    println(name)
-//    println(source.kotlin.srcDirs)
+//repositories {
+//    mavenLocal()
 //}
